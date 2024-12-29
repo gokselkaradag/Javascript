6b94a8d340333c2941a804e0069da7c281b61da7
Göksel Karadağ <goksel.karadag34@gmail.com>
2024-12-29 15:57:57 +0300

    first commit

---
+++ 3.Javascript Çıktı Verme/app.js
@@ -0,0 +1,25 @@
+//Document write ile çıktı verme 
+
+document.write("Hello World <br>");
+document.writeln("Göksel Karadağ");
+
+//console log metodu kullanımı
+
+console.log("Hello World");
+console.log({name:"Göksel",surname:"Karadağ"});
+
+//Alert popup ile çıktı verme
+
+console.log(window);
+
+alert("Hoşgeldiniz");
+
+/*
+
+    BETTER COMMENTS
+    ? Bu bir bilgilendirme yorumudur.
+    ! Burası çok önemlidir dikkat et.
+    TODO : Burası şöyle yapılması gerekiyor.
+    * Test mesaj...
+
+*/
