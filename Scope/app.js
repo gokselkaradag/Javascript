29eb4b6d8a12909742dfcbbfda4961cbd633dd77
Göksel Karadağ <goksel.karadag34@gmail.com>
2025-01-01 16:56:05 +0300

    Scope

---
+++ Scope/app.js
@@ -0,0 +1,46 @@
+/*
+    SCOPE (KAPSAM)
+
+    *Global Scope (Her yerden erişilebilir)
+    *Function Scope (Bulunduğu method içinde erişebilir)
+    *Block Scope (Method içinde döngüler)
+
+*/
+
+
+/* Global Scope */
+
+// var a = 5;
+
+// if(true){
+    
+// }
+
+// function method(){
+//     console.log(a);
+// }
+
+// method();
+
+
+/* Function Scope */
+
+// function method(){
+//     var sayi = 10;
+//     console.log(sayi);
+// }
+
+// method();
+
+
+/* Block Scope */
+
+// function method(){
+
+//     var a = 5; //function scope
+
+//     if(true){
+//         var b = 10; //block scope
+//     }
+
+// }
