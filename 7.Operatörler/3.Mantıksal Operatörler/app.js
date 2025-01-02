ff9ee5705244f3f11b65e52bd8c9411548897c81
Göksel Karadağ <goksel.karadag34@gmail.com>
2025-01-02 17:47:52 +0300

    Logical_Operators

---
+++ 7.Operatörler/3.Mantıksal Operatörler/app.js
@@ -0,0 +1,29 @@
+// -----------MANTIKSAL OPERATÖRLER-----------
+
+/*
+    && : ve
+    || : veya
+    !  : değil
+*/
+
+// Yaşınız 18'den büyükse VE 3000 tl para olması
+
+/* && */
+
+// let age = 17;
+// let money = 3500;
+
+// console.log(age > 18 && money > 3000);
+
+
+/* || */
+
+// let age = 17;
+// let money = 3500;
+
+// console.log(age > 18 || money > 3000);
+
+/* ! (Zıttını Alır) */ 
+
+// console.log(!(5>3)); //false
+// console.log((5>3)); //true
