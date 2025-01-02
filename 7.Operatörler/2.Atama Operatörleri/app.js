323e844b3d627878a012c6e44dc67090ec05b6a9
Göksel Karadağ <goksel.karadag34@gmail.com>
2025-01-02 17:38:30 +0300

    Operators_Assignment

---
+++ 7.Operatörler/2.Atama Operatörleri/app.js
@@ -0,0 +1,49 @@
+// ----------- ATAMA OPERATÖRLERİ -----------
+
+/*
+    =
+    ==
+    ===
+    +=
+    -=
+    *=
+    /=
+    %=
+    **=
+*/
+
+// = 
+
+// let a = 5;
+
+// == : eşittir
+
+//console.log(5==4);
+
+// === : Hem tiplerin hem de değerlerin eşit olup olmadıklarını kontrol eder.
+
+// console.log(5==="4");
+
+// += / -= / *= / "/="
+
+// let number = 4;
+// number += 2;
+// number -= 2;
+// number /= 2;
+// number *= 2;
+
+// console.log(number);
+
+// %= (Kalanı)
+
+// let number = 5;
+// number %= 2;
+
+// console.log(number);
+
+// **= (Karesi)
+
+// let number = 5;
+// number **= 2;
+
+// console.log(number);
