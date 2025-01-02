dd351fe664bd54a702d60b892e1147065fa2c40f
Göksel Karadağ <goksel.karadag34@gmail.com>
2025-01-02 17:14:49 +0300

    Veri Tipleri

---
+++ 6.Veri Tipleri/app.js
@@ -0,0 +1,59 @@
+/*
+? Veri Tipleri
+
+1-String
+2-Number
+3-Boolean
+4-Null
+5-Undefined
+6-Object
+7-Function
+
+*/
+
+/*--- String ---*/
+
+// let name = "Göksel";
+// console.log(name);
+// console.log(typeof name);
+
+/*--- Number ---*/
+
+// let number = 10.8;
+// console.log(number);
+// console.log(typeof number);
+
+/*--- Boolean (True ve False'den oluşur) ---*/
+
+// console.log(5>2);
+// console.log(5>8);
+
+/*--- Null ---*/
+
+// let a = null;
+// a = 25;
+// console.log(a);
+
+/*--- Undefined ---*/
+
+// let a;
+// console.log(a);
+
+/*--- Object ---*/
+
+// let human ={
+//     name : "Göksel",
+//     surname : "Karadağ",
+//     age : 25
+// }
+
+// console.log(human);
+
+/*--- Function ---*/
+
+// let func = function(){
+//     console.log("Merhaba");
+// }
+
+// func();
+// console.log(typeof func);
