0afff7efb73d45864907b458df18153e6bc792ff
Göksel Karadağ <goksel.karadag34@gmail.com>
2025-01-02 17:27:06 +0300

    Operators_Arithmetic

---
+++ 7.Operatörler/1.Aritmetik Operatörler/app.js
@@ -0,0 +1,62 @@
+// ARİTMETİK OPERATÖRLER
+
+/* ------------------>  = atama operatörü önemli !
+    +  
+    -  
+    *  
+    /  
+    %  
+    ++  
+    --  
+    **  
+*/
+
+
+// +
+
+// let a = 5;
+// let b = 5;
+
+// console.log(a+b);
+
+// -
+
+// let a = 10;
+// let b = 6;
+
+// console.log(a-b);
+
+// *
+
+// let a = 10;
+// let b = 6;
+
+// console.log(a*b);
+
+// "/"
+
+// let a = 10;
+// let b = 5;
+
+// console.log(a/b);
+
+// % (Mod alma )
+
+// let a = 10;
+// let b = 20;
+
+// console.log(a%b);
+
+// ++ (1 arttırmak için) --  (1 azaltmak için)
+
+// let a = 10;
+// a++;
+// console.log(a);
+
+// let b = 10;
+// b--;
+// console.log(b);
+
+// ** (Üslü Sayı)
+
+// console.log(2**3);
