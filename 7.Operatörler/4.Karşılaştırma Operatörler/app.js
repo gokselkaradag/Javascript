74f55c43aa671fbf53efd4a57ffc6301f507a823
Göksel Karadağ <goksel.karadag34@gmail.com>
2025-01-06 18:41:38 +0300

    Comparison_Operators

---
+++ 7.Operatörler/4.Karşılaştırma Operatörler/app.js
@@ -0,0 +1,20 @@
+// -----KARŞILAŞTIRMA OPERATÖRLERİ-------
+
+/*
+
+    ==  : eşit mi
+    !=  : eşit değil mi
+    >   : büyük mü
+    <   : küçük mü
+    >=  : büyük eşit mi
+    <=  : küçük eşit mi
+
+*/
+
+console.log(5!=3);
+
+let age = 23;
+console.log(age!=23);
+
+console.log(12<=12);
+
