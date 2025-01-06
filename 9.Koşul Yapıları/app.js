a9ebbda199e5e209fd9fb8c08bf135290277c654
Göksel Karadağ <goksel.karadag34@gmail.com>
2025-01-06 19:06:00 +0300

    İf&Else

---
+++ 9.Koşul Yapıları/app.js
@@ -0,0 +1,19 @@
+// KOŞUL YAPILARI
+
+// let not = 35;
+
+// if(not > 50){
+//     console.log("Geçtiniz , notunuz:" + not);
+// }else{
+//     console.log("Kaldınız , notunuz:" + not);
+// }
+
+
+// let age = prompt("Yaşınız :");
+// let money = prompt("Bütçeniz :");
+
+// if(age >= 18 && money >= 3000){
+//     alert("Ehliyet Alabilirsiniz...");
+// }else{
+//     alert("Ehliyet Alamazsınız!!!");
+// }
