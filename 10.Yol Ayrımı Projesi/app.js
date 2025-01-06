4aecbee8738c0bf1735582cf5521c1cec52d0ba3
Göksel Karadağ <goksel.karadag34@gmail.com>
2025-01-06 19:33:32 +0300

    Crossroads_Project

---
+++ 10.Yol Ayrımı Projesi/app.js
@@ -0,0 +1,14 @@
+let path = prompt("Lütfen gitmek istediğiniz yolu seçiniz :");
+
+if(path == 1){
+    alert("Seçilen yol " + path + ". yoldur.");
+}
+else if(path == 2){
+    alert("Seçilen yol " + path + ". yoldur.");
+}
+else if(path == 3){
+    alert("Seçilen yol " + path + ". yoldur.");
+}
+else{
+    alert("Lütfen geçerli bir yol seçiniz !!");
+}
