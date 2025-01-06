45664a1e389806d9d7c19b69032eab4114739b2b
Göksel Karadağ <goksel.karadag34@gmail.com>
2025-01-06 18:53:14 +0300

    Dialogue_Boxes

---
+++ 8.Diyalog Kutuları/app.js
@@ -0,0 +1,19 @@
+/*
+    -------DİYALOG KUTULARI-------
+    - Alert
+    - Prompt
+    - Confirm
+*/
+
+console.log(window);
+
+// alert("Naber...");
+
+// Prompt(); Kullanıcıdan değer alabiliyoruz...
+
+// Kullanıcıdan alınan değerler her zaman string döner...
+// let name = prompt("Adınızı Giriniz:")
+// console.log(name);
+
+// True & False işlemleri...
+// confirm("Silmek istediğinize emin misiniz ?");
